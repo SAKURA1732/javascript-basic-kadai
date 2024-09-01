c43dabaaf15ed73f47d849b6f364a20ec7d6d1b5
SAKURA1732 <ymdakk8490@gmail.com>
2024-09-02 05:00:51 +0900

    ファイルを追加

---
+++ kadai_005/bmi.js
@@ -0,0 +1,12 @@
+//　変数の宣言
+let bodyWeight;
+let bodyHeight;
+
+//　値の代入
+bodyWeight = '68';
+bodyHeight = '1.7';
+
+const bmi = bodyWeight / (bodyHeight * bodyHeight);
+
+//　コンソールへの出力
+console.log(bmi);

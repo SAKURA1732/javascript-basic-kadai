53a94158eb7f154ffed80c4181ce7988fe71f80a
SAKURA1732 <ymdakk8490@gmail.com>
2024-09-08 15:05:41 +0900

    add kadai_014

---
+++ kadai_014/square.js
@@ -0,0 +1,8 @@
+// 数値を二乗する関数
+const calculateSquared = (number) => {
+    // 数値を二乗してその結果を戻り値として返す
+    return number * number;
+  };
+  
+  const result = calculateSquared(10);
+  console.log(result); 
